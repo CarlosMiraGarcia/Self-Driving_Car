ff66b53b2122868a561fc9e0240ab352b9533810
Carlos Mira <carlosgarciamira@gmail.com>
2022-03-30 14:32:42 +0100

    New files

--- src/main.adb
+++ src/main.adb
@@ -1,10 +1,18 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with dashboard_warning_lights; use dashboard_warning_lights;
 with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
+with Ada.Text_IO.Text_Streams; use Ada.Text_IO.Text_Streams;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Text_IO.Unbounded_IO;
 
 procedure Main is
    Dashboard : dashboard_warning_lights.Lights;
-
+   type Lines is
+      record
+         Line  : Ada.Strings.Unbounded.Unbounded_String;
+      end record;
+   type LinesArray is array (Natural range <>) of Lines;
+   Line : Unbounded_String;
    procedure Clear is
    begin
       Put (ASCII.ESC);
@@ -33,6 +41,7 @@ procedure Main is
    end LightsOn;
 
    procedure StartingCar is
+
    type Index is range 1..14;
    begin
       for i in 0..2 loop
@@ -73,11 +82,55 @@ procedure Main is
       end loop;
    end StartingCar;
 
+   procedure Read_File is
+      File_Input : File_Type;
+      Lines_Array : LinesArray(1..310);
+      SeparatorLine : String := "________________________________________";
+   begin
+      Open (File => File_Input,
+            Mode => In_File,
+            Name => "road.txt");
+      Clear;
+
+      for i in Lines_Array'Range loop
+         Lines_Array(i).Line := Ada.Text_IO.Unbounded_IO.Get_Line(File_Input);
+      end loop;
+
+      for i in Lines_Array'Range loop
+         if i + 30 <= 310 then
+            Line := Lines_Array(i).Line;
+
+            for k in 1..10 loop
+               if Element (Line, k) = '#' then
+                  Replace_Element(Line, k+11, 'C');
+                  Replace_Element(Line, k+12, 'A');
+                  Replace_Element(Line, k+13, 'R');
+                  Ada.Text_IO.Put_Line(SeparatorLine);
+                  Ada.Text_IO.Put_Line("");
+                  Ada.Text_IO.Unbounded_IO.Put_Line(Line);
+                  exit;
+               end if;
+            end loop;
+            for j in 1..30 loop
+               Ada.Text_IO.Unbounded_IO.Put_Line(Lines_Array(i + j).Line);
+            end loop;
+            Ada.Text_IO.Put_Line(SeparatorLine);
+            delay 0.02;
+         else
+            delay 0.5;
+            exit;
+         end if;
+         Clear;
+      end loop;
+      Close(File => File_Input);
+   end Read_File;
+
 begin
    --  StartingCar;
    --  delay 0.5;
-   LightsOn;
-   Print;
-   delay 3.0;
+   Read_File;
+   --  LightsOn;
+   --  Print;
+   --  delay 3.0;
 
 end Main;
