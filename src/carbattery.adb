9ca6d5768664c43751c1d881b144c44d63795cd8
Carlos Mira <carlosgarciamira@gmail.com>
2022-04-03 14:58:49 +0100

    Added some variables

--- src/carbattery.adb
+++ src/carbattery.adb
@@ -2,27 +2,20 @@ package body carbattery with SPARK_Mode is
 
    procedure ChargeBattery (This : in out Battery) is
    begin
-      if This.charge < MaxCharge then
-         This.charge := This.charge + DischargeRatio;
-         This.charging := True;
-      else
-         This.charging := False;
-      end if;
+      This.charge := This.charge + DischargeRatio;
+
    end ChargeBattery;
 
    procedure UseBattery (This : in out Battery) is
    begin
-      if This.charge >= DischargeRatio and This.charging = False
-        and This.charge <= BatteryCharge'Last then
-         This.charge := This.charge - DischargeRatio;
-      end if;
+      This.charge := This.charge - DischargeRatio;
    end UseBattery;
 
    function CreateBattery return Battery is
       result : Battery;
    begin
-      result := (charge => (5),
-                 charging => (False));
+      result := (charge => (15),
+                 charging => (Off));
       return result;
    end CreateBattery;
 
--- src/carbattery.ads
+++ src/carbattery.ads
@@ -3,24 +3,25 @@ with helpers; use helpers;
 
 package carbattery with SPARK_Mode is
 
-   type BatteryCharge is range 0..5;
+   type Status is (On, Off);
+
+   type BatteryCharge is range 0..15;
    DischargeRatio : constant BatteryCharge := 1;
    Threshold : constant BatteryCharge := (BatteryCharge'Last - DischargeRatio);
-   MaxCharge: constant BatteryCharge := 5;
-   MinCharge: constant BatteryCharge := 0;
+   MinCharge: constant BatteryCharge := DischargeRatio;
+   MaxCharge: constant BatteryCharge := BatteryCharge'Last;
 
-   type Battery is tagged record
+   type Battery is record
       charge : BatteryCharge;
-      charging : Boolean;
+      charging : Status;
    end record;
 
    procedure ChargeBattery (This : in out Battery) with
-     Pre'Class => This.charge < MaxCharge,
+     Pre => This.charge < BatteryCharge'Last,
      Post => This.charge = This.charge'Old + DischargeRatio;
 
    procedure UseBattery (This : in out Battery) with
-     Pre'Class => This.charge > MinCharge and This.charging = False and
-     This.charge <= MaxCharge and MinCharge < MaxCharge,
+     Pre => This.charge > MinCharge and This.charging = Off and This.charge <= MaxCharge,
      Post => This.charge = This.charge'Old - DischargeRatio;
 
    function CreateBattery return Battery;
