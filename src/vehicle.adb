93d855c57d364703d80b037fa45838b930c69f36
Carlos Mira <carlosgarciamira@gmail.com>
2022-04-06 13:39:19 +0100

    Changed procedures to comply with SPARK and the changes in helper

--- src/vehicle.adb
+++ src/vehicle.adb
@@ -18,67 +18,100 @@ package body vehicle with SPARK_Mode is
    procedure StartingCar (This : in out Car) is
       type Index is range 1..15;
    begin
-      This.dashboardLights.lights(ReadyToDrive).state := On;
+      if (for all i in This.dashboardLights.lights'Range => This.dashboardLights.lights(i).state = Off) then
+         This.dashboardLights.lights(ReadyToDrive).state := On;
+      else
+         PrintError("   Run the diagnosis tool to fix the problems   ");
+      end if;
       This.carStatus := On;
    end StartingCar;
 
-   procedure GearAdvancing (This : in out Car) is
+   procedure GearForward (This : in out Car) is
    begin
-      This.gearStatus := Advancing;
-   end GearAdvancing;
+      This.gearStatus := Forward;
+      if This.carStatus = On and This.dashboardLights.lights(ReadyToDrive).state = On then
+         This.accelerating := True;
+      elsif (for some i in This.dashboardLights.lights'Range => This.dashboardLights.lights(i).state = Error) then
+         PrintError("   Run the diagnosis tool to fix the problems   ");
+      end if;
+   end GearForward;
 
    procedure GearReversing (This : in out Car) is
    begin
       This.gearStatus := Reversing;
+      if This.carStatus = On and This.dashboardLights.lights(ReadyToDrive).state = On  then
+         This.accelerating := True;
+      elsif (for some i in This.dashboardLights.lights'Range => This.dashboardLights.lights(i).state = Error) then
+         PrintError("   Run the diagnosis tool to fix the problems   ");
+      end if;
    end GearReversing;
 
    procedure GearParked (This : in out Car) is
    begin
       This.gearStatus := Parked;
+      if This.carStatus = On then
+         This.accelerating := False;
+      end if;
    end GearParked;
 
    procedure PlugBattery (This : in out Car) is
    begin
-      if this.carBattery.charge < BatteryCharge'Last then
-         ChargeBattery(This.carBattery);
-         This.dashboardLights.lights(Charging).state := On;
-         This.carBattery.charging := On;
-      elsif This.carBattery.charge = BatteryCharge'Last then
-         This.dashboardLights.lights(LowBattery).state := Off;
-         This.dashboardLights.lights(Charging).state := Off;
-         This.carBattery.charging := Off;
+      if This.gearStatus = Parked then
+         if this.carBattery.charge < BatteryCharge'Last then
+            ChargeBattery(This.carBattery);
+            This.dashboardLights.lights(Charging).state := On;
+            This.dashboardLights.lights(ReadyToDrive).state := Off;
+            This.carBattery.charging := On;
+         elsif This.carBattery.charge = BatteryCharge'Last then
+            This.dashboardLights.lights(LowBattery).state := Off;
+            This.dashboardLights.lights(Charging).state := Off;
+            This.carBattery.charging := Off;
+            if This.carStatus = On then
+               This.dashboardLights.lights(ReadyToDrive).state := On;
+            end if;
+         end if;
+      else
+         PrintError("Gear needs to be set to Parked before charging");
+         delay(2.0);
       end if;
    end PlugBattery;
 
    procedure Drive (This : in out car) is
    begin
       if This.carStatus = On then
-         UseBattery(This.carBattery);
-         if Integer(This.carBattery.charge) - Integer(This.speed) >= 1 then
+         if This.accelerating = True and This.speed <= This.currentRoad.speed_limit
+           and Integer(This.carBattery.charge) - Integer(This.speed) > 1 then
+            UseBattery(This.carBattery);
             if This.speed < This.currentRoad.speed_limit then
                Accelerate(This);
             end if;
-         else
-            Put_Line(HT & "Battery low. Looking for a place to charge");
-            This.dashboardLights.lights(LowBattery).state := On;
+         elsif This.accelerating = False then
             if This.speed > SpeedRange'First then
+               UseBattery(This.carBattery);
                Decelerate(This);
-            else
+            elsif This.speed = 0 then
                This.gearStatus := Parked;
                This.dashboardLights.lights(ReadyToDrive).state := Off;
                This.carStatus := Off;
             end if;
          end if;
+         if Integer(This.carBattery.charge) - Integer(This.speed) <= 1 then
+            if This.accelerating = True then
+               PrintError("      Not enough battery. Reducing speed now    ");
+            end if;
+            This.dashboardLights.lights(LowBattery).state := On;
+            This.accelerating := False;
+         end if;
       else
-         Put_Line(HT & "The car needs to be on.");
-         delay(2.0);
+         PrintError("            The car needs to be on            ");
       end if;
    end Drive;
 
-      --  if This.carStatus = On and This.carBattery.charge >= DischargeRatio and This.carBattery.charging = False
-      --    and (This.gearStatus = Advancing or This.gearStatus = Reversing) then
-      --  elsif This.carBattery.charge < DischargeRatio then
-      --     This.carStatus := Off;
-      --     This.dashboardLights.lights(ReadyToDrive).state := Off;
-      --  end if;
+   procedure DiagnosisTool (This : in Car) is
+   begin
+      for light in This.dashboardLights.lights'Range loop
+         Put_Line ("State of " & light'Image & " is " & This.dashboardLights.lights(light).state'Image);
+      end loop;
+   end DiagnosisTool;
+
 end vehicle;
--- src/vehicle.ads
+++ src/vehicle.ads
@@ -1,12 +1,13 @@
 
+with Ada.Strings.Unbounded;  use Ada.Strings.Unbounded;
 with dashboard_warning_lights; use dashboard_warning_lights;
 with road; use road;
 with carbattery; use carbattery;
 
 package vehicle with SPARK_Mode is
 
-   type Gear is (Parked, Advancing, Reversing);
-   type Status is (On, Off);
+   type Gear is (Parked, Forward, Reversing);
+   type Accelerator is (Yes, No);
 
    type Car is record
       speed : SpeedRange := 0;
@@ -15,14 +16,18 @@ package vehicle with SPARK_Mode is
       currentRoad : road.Road := CreateRoad;
       gearStatus : Gear := Parked;
       carStatus : Status := Off;
+      accelerating : Boolean := False;
    end record;
+   StringError : Unbounded_String;
 
    procedure Accelerate (This : in out Car) with
-     Pre => This.speed <= SpeedRange'Last and This.speed < This.currentRoad.speed_limit,
+     Pre => This.speed <= SpeedRange'Last and This.speed < This.currentRoad.speed_limit
+     and (This.gearStatus = Forward or This.gearStatus = Reversing) and This.accelerating = True,
      Post => This.speed = This.speed'Old + 1;
 
    procedure Decelerate (This : in out Car) with
-     Pre => This.speed > SpeedRange'First,
+     Pre => This.speed > SpeedRange'First and (This.gearStatus = Forward or This.gearStatus = Reversing)
+     and This.accelerating = False,
      Post => This.speed = This.speed'Old - 1;
 
    procedure StartingCar (This: in out Car) with
@@ -30,12 +35,12 @@ package vehicle with SPARK_Mode is
      and (for all i in This.dashboardLights.lights'Range => This.dashboardLights.lights(i).state = Off),
      Post => This.carStatus = On;
 
-   procedure GearAdvancing (This: in out Car) with
-     Pre => This.speed = 0,
-     Post => This.gearStatus = Advancing;
+   procedure GearForward (This: in out Car) with
+     Pre => This.speed = 0 and This.carBattery.charging = Off and This.dashboardLights.lights(ReadyToDrive).state = On,
+     Post => This.gearStatus = Forward;
 
    procedure GearReversing (This: in out Car) with
-     Pre => This.speed = 0,
+     Pre => This.speed = 0 and This.carBattery.charging = Off and This.dashboardLights.lights(ReadyToDrive).state = On,
      Post => This.gearStatus = Reversing;
 
    procedure GearParked (This: in out Car) with
@@ -43,12 +48,14 @@ package vehicle with SPARK_Mode is
      Post => This.gearStatus = Parked;
 
    procedure PlugBattery (This : in out Car) with
-     Pre => This.gearStatus = Parked and This.carBattery.charge < This.carBattery.MaxCharge and This.carStatus = Off;
+     Pre => This.gearStatus = Parked and This.carBattery.charge < This.carBattery.MaxCharge;
 
    procedure Drive (This : in out Car) with
-     Pre => (This.dashboardLights.lights(ReadyToDrive).state = On) and (This.gearStatus = Advancing or This.gearStatus = Reversing)
+     Pre => (This.dashboardLights.lights(ReadyToDrive).state = On) and (This.gearStatus = Forward or This.gearStatus = Reversing)
      and This.carBattery.charge >= DischargeRatio and This.carBattery.charge > This.carBattery.MinCharge
-     and This.carBattery.charge <= This.carBattery.MaxCharge and (This.gearStatus = Advancing or This.gearStatus = Reversing)
-     and This.carBattery.charging = Off and This.carStatus = On;
+     and This.carBattery.charge <= This.carBattery.MaxCharge and This.carBattery.charging = Off and This.carStatus = On;
+
+   procedure DiagnosisTool (This : in Car) with
+     Pre => This.carStatus = Off and This.gearStatus = Parked;
 
 end vehicle;
