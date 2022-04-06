12a3607b6a14a3d03b337fe71afa3237bc881d68
Carlos Mira <carlosgarciamira@gmail.com>
2022-04-06 16:47:44 +0100

    Created procedures to print the menus so the main loop is cleaner

--- src/main.adb
+++ src/main.adb
@@ -15,16 +15,10 @@ procedure Main is
    Ch : Character := Ada.Characters.Latin_1.LC_L;
    SeparatorLine2: String := "______________________________________________________________";
    BaterryPer : Float;
+   NewRun : Boolean := True;
 
    procedure PrintHeader is
    begin
-      Clear;
-      Put_Line (HT & SeparatorLine2);
-      Put_Line ("");
-      CheckLights(dummy_car.dashboardLights);
-      Put_Line ("");
-      Put_Line (HT & SeparatorLine2);
-      Put_Line ("");
       if dummy_car.carStatus = Off then
          Put (HT & " Car Status: " & " " & ESC & "[1;30m" & ESC & "[101m" & " " & dummy_car.carStatus'Image & " " & ESC & "[0m");
       elsif dummy_car.carStatus = On then
@@ -49,13 +43,37 @@ procedure Main is
       Put_Line ("");
       Put (HT & " Road Max Speed: " & dummy_car.currentRoad.speed_limit'Image & " mph");
       Put_Line (HT & HT & "Accelerating: " & "  " & dummy_car.accelerating'Image);
-      Put_Line (HT & " Car Speed: " & HT & "  " & dummy_car.speed'Image & " mph");
+      Put_Line (HT & " Car Speed: " & HT & " " & dummy_car.speed'Image & " mph");
 
 
       Put_Line (HT & SeparatorLine2);
       Put_Line ("");
       Put_Line ("");
    end PrintHeader;
+
+   procedure PrintMenu is
+   begin
+      Put_Line (HT & "Type one of the following options and press Enter:");
+      Put_Line ("");
+      Put_Line (HT & ESC & "[102m" & " s " & ESC & "[0m" & " to (s)tart/(s)top the car." & HT & HT &"        _______");
+      Put_Line (HT & ESC & "[102m" & " p " & ESC & "[0m" & " to change gear to (p)arked." & HT & "               //  ||\ \");
+      Put_Line (HT & ESC & "[102m" & " f " & ESC & "[0m" & " to change gear to (f)orward." & HT & " _____//___||_\ \___");
+      Put_Line (HT & ESC & "[102m" & " r " & ESC & "[0m" & " to change gear to (r)eversing." & HT & " )  _          _    \");
+      Put_Line (HT & ESC & "[102m" & " c " & ESC & "[0m" & " to (c)harge the car." & HT & HT & " |_/ \________/ \___|");
+      Put_Line (HT & ESC & "[102m" & " d " & ESC & "[0m" & " to run the (d)iagnosis tool." & "      _____\_/________\_/____");
+      Put_Line (HT & ESC & "[102m" & " e " & ESC & "[0m" & " to (e)xit the program." & HT & "      Art by Colin Douthwaite");
+   end PrintMenu;
+
+   procedure PrintDiagnosisMenu is
+   begin
+      Put_Line (HT & "Type one of the following options and press Enter:");
+      Put_Line ("");
+      Put_Line (HT & ESC & "[102m" & " s " & ESC & "[0m" & " to (s)tart the diagnosis.");
+      Put_Line (HT & ESC & "[102m" & " f " & ESC & "[0m" & " to (f)ix the problems.");
+      Put_Line (HT & ESC & "[102m" & " b " & ESC & "[0m" & " to go (b)ack.");
+      Put_Line (HT & ESC & "[102m" & " e " & ESC & "[0m" & " to (e)xit the program.");
+      delay(0.5);
+   end PrintDiagnosisMenu;
 
    procedure RandomFault (This : in out Dashboard) is
       package Rand_Int is new ada.numerics.discrete_random(randRange);
@@ -68,7 +86,7 @@ procedure Main is
       NumRandom := Random(Gene);
       for i in This.lights'Range loop
          if Index = Integer(NumRandom) then
-            This.lights(i).state := Error;
+            This.lights(i).error := On;
          end if;
          Index := Index + 1;
       end loop;
@@ -91,96 +109,112 @@ procedure Main is
 begin
    dummy_car.currentRoad.SetSpeedLimit(20);
    loop
-      PrintHeader;
-      Put_Line (HT & "Type one of the following options and press Enter:");
-      Put_Line (HT & HT & HT & HT & HT &  HT &"        _______");
-      Put_Line (HT & ESC & "[102m" & " s " & ESC & "[0m" & " to (s)tar/(s)top the car." & HT & "               //  ||\ \");
-      Put_Line (HT & ESC & "[102m" & " p " & ESC & "[0m" & " to change gear to (p)arked." & HT & HT & " _____//___||_\ \___");
-      Put_Line (HT & ESC & "[102m" & " f " & ESC & "[0m" & " to change gear to (f)orward." & HT & " )  _          _    \");
-      Put_Line (HT & ESC & "[102m" & " r " & ESC & "[0m" & " to change gear to (r)eversing." & HT & " |_/ \________/ \___|");
-      Put_Line (HT & ESC & "[102m" & " c " & ESC & "[0m" & " to (c)harge the car." & HT & "      _____\_/________\_/____");
-      Put_Line (HT & ESC & "[102m" & " e " & ESC & "[0m" & " to (e)xit the program." & HT & "      Art by Colin Douthwaite");
-      delay(0.5);
-      if Ch = Ada.Characters.Latin_1.LC_S then
-         if dummy_car.carBattery.charge = BatteryCharge'First then
-            PrintError ("  There is not enough battery to start the car ");
-            delay(2.0);
-         elsif dummy_car.carStatus = Off and dummy_car.gearStatus = Parked and dummy_car.carBattery.charging = Off then
-            StartingCar(dummy_car);
-            RandomFault(dummy_car.dashboardLights);
-            RandomFault(dummy_car.dashboardLights);
-         elsif dummy_car.carStatus = On then
-            if dummy_car.speed > SpeedRange'First then
-               dummy_car.accelerating := False;
-            else
-               LightsOff(dummy_car.dashboardLights);
-               dummy_car.carStatus := Off;
+      Clear;
+      Put_Line (HT & SeparatorLine2);
+      Put_Line ("");
+      CheckLights(dummy_car.dashboardLights);
+      Put_Line ("");
+      Put_Line (HT & SeparatorLine2);
+      Put_Line ("");
+      if dummy_car.diagnosis then
+         PrintDiagnosisMenu;
+         if Ch = Ada.Characters.Latin_1.LC_S then
+            DiagnosisTool (dummy_car);
+            delay(1.0);
+         elsif Ch = Ada.Characters.Latin_1.LC_F then
+            FixProblems (dummy_car);
+         elsif Ch = Ada.Characters.Latin_1.LC_B then
+            dummy_car.diagnosis := False;
+            Clear;
+         end if;
+      elsif not dummy_car.diagnosis then
+         PrintHeader;
+         PrintMenu;
+         delay(0.5);
+         if Ch = Ada.Characters.Latin_1.LC_S then
+            if dummy_car.carBattery.charge = BatteryCharge'First then
+               PrintError ("  There is not enough battery to start the car ");
+            elsif dummy_car.carStatus = Off and dummy_car.gearStatus = Parked and dummy_car.carBattery.charging = Off then
+               if NewRun then
+                  RandomFault(dummy_car.dashboardLights);
+                  NewRun := False;
+               end if;
+               StartingCar(dummy_car);
+            elsif dummy_car.carStatus = On then
+               if dummy_car.speed > SpeedRange'First then
+                  dummy_car.accelerating := False;
+               else
+                  LightsOff(dummy_car.dashboardLights);
+                  dummy_car.carStatus := Off;
+               end if;
+            elsif dummy_car.gearStatus /= Parked then
+               PrintError ("  To start the car, the gear must be on Parked  ");
+            elsif dummy_car.carBattery.charging = On then
+               PrintError ("     Wait until the battery is fully charged    ");
             end if;
-         elsif dummy_car.gearStatus /= Parked then
-            PrintError ("  To start the car, the gear must be on Parked  ");
-         elsif dummy_car.carBattery.charging = On then
-            PrintError ("     Wait until the battery is fully charged    ");
+         elsif Ch = Ada.Characters.Latin_1.LC_P then
+            if dummy_car.carStatus = Off and dummy_car.speed = 0 then
+               GearParked(dummy_car);
+            elsif dummy_car.speed > 0 then
+               PrintError("     Speed must be 0 in order to change gear    ");
+            end if;
+         elsif Ch = Ada.Characters.Latin_1.LC_F then
+            if dummy_car.carStatus = On and dummy_car.speed = 0 and
+              (for all light in dummy_car.dashboardLights.lights'Range
+               => dummy_car.dashboardLights.lights(light).error = Off) then
+               GearForward(dummy_car);
+            elsif dummy_car.carStatus = On and dummy_car.dashboardLights.lights(LowBattery).state = On then
+               PrintError(" There is not enough battery. Charge it first   ");
+            elsif dummy_car.carStatus = Off then
+               GearForward(dummy_car);
+            elsif dummy_car.speed > 0 then
+               PrintError("     Speed must be 0 in order to change gear    ");
+            elsif (for some light in dummy_car.dashboardLights.lights'Range
+                   => dummy_car.dashboardLights.lights(light).error = Off) then
+               PrintError("      Errors found. Run the diagnosis tool      ");
+            end if;
+         elsif Ch = Ada.Characters.Latin_1.LC_R then
+            if dummy_car.carStatus = On  and dummy_car.speed = 0  and
+              (for all light in dummy_car.dashboardLights.lights'Range
+               => dummy_car.dashboardLights.lights(light).error = Off) then
+               GearReversing(dummy_car);
+            elsif dummy_car.carStatus = On and dummy_car.dashboardLights.lights(LowBattery).state = On then
+               PrintError(" There is not enough battery. Charge it first   ");
+            elsif dummy_car.carStatus = Off then
+               GearReversing(dummy_car);
+            elsif dummy_car.speed > 0 then
+               PrintError("     Speed must be 0 in order to change gear    ");
+            elsif (for some light in dummy_car.dashboardLights.lights'Range
+                   => dummy_car.dashboardLights.lights(light).error = Off) then
+               PrintError("      Errors found. Run the diagnosis tool      ");
+            end if;
+         elsif Ch = Ada.Characters.Latin_1.LC_C then
+            if dummy_car.carBattery.charge < BatteryCharge'Last and dummy_car.gearStatus = Parked then
+               dummy_car.carBattery.charging := On;
+            elsif dummy_car.carBattery.charge = BatteryCharge'Last and dummy_car.gearStatus = Parked then
+               PrintError("  Cannot charge the battery because it is full  ");
+            elsif dummy_car.gearStatus /= Parked then
+               PrintError("    To start charging, set the gear to Parked   ");
+            end if;
+         elsif Ch = Ada.Characters.Latin_1.LC_D then
+            if dummy_car.carStatus = On then
+               dummy_car.diagnosis := True;
+            else
+               PrintError("    The car must be on to start the diagnosis   ");
+            end if;
          end if;
-      elsif Ch = Ada.Characters.Latin_1.LC_P then
-         if dummy_car.carStatus = Off and dummy_car.speed = 0 then
-            GearParked(dummy_car);
-         elsif dummy_car.speed > 0 then
-            PrintError("     Speed must be 0 in order to change gear    ");
+         if (dummy_car.gearStatus = Forward or dummy_car.gearStatus = Reversing)
+           and dummy_car.dashboardLights.lights(ReadyToDrive).state = On then
+            Drive(dummy_car);
          end if;
-      elsif Ch = Ada.Characters.Latin_1.LC_F then
-         if dummy_car.carStatus = On and dummy_car.speed = 0 and
-           (for all light in dummy_car.dashboardLights.lights'Range
-            => dummy_car.dashboardLights.lights(light).state /= Error) then
-            GearForward(dummy_car);
-         elsif dummy_car.carStatus = On and dummy_car.dashboardLights.lights(LowBattery).state = On then
-            PrintError(" There is not enough battery. Charge it first   ");
-         elsif dummy_car.carStatus = Off then
-            GearForward(dummy_car);
-         elsif dummy_car.speed > 0 then
-            PrintError("     Speed must be 0 in order to change gear    ");
-         elsif (for some light in dummy_car.dashboardLights.lights'Range
-                => dummy_car.dashboardLights.lights(light).state /= Error) then
-            PrintError("      Errors found. Run the diagnosis tool      ");
+         if dummy_car.carBattery.charging = On then
+            PlugBattery(dummy_car);
          end if;
-      elsif Ch = Ada.Characters.Latin_1.LC_R then
-         if dummy_car.carStatus = On  and dummy_car.speed = 0  and
-           (for all light in dummy_car.dashboardLights.lights'Range
-            => dummy_car.dashboardLights.lights(light).state /= Error) then
-            GearReversing(dummy_car);
-         elsif dummy_car.carStatus = On and dummy_car.dashboardLights.lights(LowBattery).state = On then
-            PrintError(" There is not enough battery. Charge it first   ");
-         elsif dummy_car.carStatus = Off then
-            GearReversing(dummy_car);
-         elsif dummy_car.speed > 0 then
-            PrintError("     Speed must be 0 in order to change gear    ");
-         elsif (for some light in dummy_car.dashboardLights.lights'Range
-                => dummy_car.dashboardLights.lights(light).state /= Error) then
-            PrintError("      Errors found. Run the diagnosis tool      ");
-         end if;
-      elsif Ch = Ada.Characters.Latin_1.LC_C then
-         if dummy_car.carBattery.charge < BatteryCharge'Last and dummy_car.gearStatus = Parked then
-            dummy_car.carBattery.charging := On;
-         elsif dummy_car.carBattery.charge = BatteryCharge'Last and dummy_car.gearStatus = Parked then
-            PrintError("  Cannot charge the battery because it is full  ");
-         elsif dummy_car.gearStatus /= Parked then
-            PrintError("    To start charging, set the gear to Parked   ");
-         end if;
-      elsif Ch = Ada.Characters.Latin_1.LC_E then
+      end if;
+      if Ch = Ada.Characters.Latin_1.LC_E then
          Clear;
          exit;
       end if;
-      if (dummy_car.gearStatus = Forward or dummy_car.gearStatus = Reversing)
-        and dummy_car.dashboardLights.lights(ReadyToDrive).state = On then
-         Drive(dummy_car);
-      elsif dummy_car.carBattery.charging = On then
-         PlugBattery(dummy_car);
-      end if;
    end loop;
-
-   --  delay 0.5;
-   -- Read_File;
-   -- dashboard_warning_lights.LightsOn;
-   --  Print;
-   --  delay 3.0;
 
 end Main;
